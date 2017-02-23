5849463de242d9399829a848f4a7c4c263d60548
hiroshi <sisakux@gmail.com>
2017-02-23 13:34:53 +0900

    修正など

--- Lesson/3.Repetitive Processing/Print-Test-Cases.rb
+++ Lesson/3.Repetitive Processing/Print-Test-Cases.rb
@@ -1,5 +1,14 @@
 #Print Test Cases
-
+i = 0
+while true
+  x = gets.to_i
+  i += 1
+  break if x == 0
+  puts "Case #{i}: #{x}"
+end
 =begin
 条件付きでループをブレイクすれば良いのかな
-putsはeachでループさせれば良いかな
+putsはeachでループさせればいいのか？
+問題文を読んだら、入力毎に出力っていう形で良いらしい
+サンプルが分かり辛いよー
+=end
---
+++ Lesson/3.Repetitive Processing/Swapping-Two-Numbers.rb
@@ -0,0 +1,10 @@
+# Swapping Two Numbers
+loop do
+    x, y = gets.split.map(&:to_i)
+    break if x == 0 && y == 0
+    if x < y
+        puts "#{x} #{y}"
+    else
+        puts "#{y} #{x}"
+    end
+end
