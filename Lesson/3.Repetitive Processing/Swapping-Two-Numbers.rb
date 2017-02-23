f7a1e14855ca27be1e03708e91e2bd0f7e61630b
hiroshi <sisakux@gmail.com>
2017-02-23 13:49:25 +0900

    修正2

--- Lesson/3.Repetitive Processing/Swapping-Two-Numbers.rb
+++ Lesson/3.Repetitive Processing/Swapping-Two-Numbers.rb
@@ -8,3 +8,18 @@ loop do
         puts "#{y} #{x}"
     end
 end
+
+=begin
+Print Test Casesと違い、今回はloop doで記載
+while tureでも行ける
+loop do <= >while tureの関係
+厳密にはスコープ範囲が違うらしいけど、あまり詳しい資料が見つからず（whileは制御構造、loop doの記載はリファレンスのKernelモジュール > loopに記載あり）
+最初書いた時、Print Test Cases（今は修正済み）でwhile単体で書いても通ったから
+そのまま記載したらエラーが出たのでloop doに変更して書いてそのまま提出したという経緯があるため
+あとで確認した所、
+while 条件 doの書き方が基本というか、これが構文なので
+それ以外だと普通はエラーが出るっぽい
+Print Test Casesとの時はたまたま
+while (x = gets.to_i) doの形になったために動いたっぽい
+なので書く時は注意だね
+=end
